66a3bb79ae4d4171a52572e2cc0cea90beb1be68
unknown <melaniemozee@gmail.com>
2020-06-22 12:38:21 -0500

    Created comments for Member variables, and started creating constructors

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    class CustomList
+    public class CustomList<T>
     {
+        // member variables - Has A
+        // index [] - where each item is located
+        // count - has how many items
+        // capacity - how many items can fit in list
+        public int index;
+        public int count;
+        public int capacity;
+
+        // constructor - Spawner
+
+        public CustomList()
+        {
+
+        }
+
+        // member methods - Can Do
+
     }
 }
