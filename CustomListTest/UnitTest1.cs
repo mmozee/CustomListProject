b4a0457e5cd0994c4f70871e5213f3d0f3030300
unknown <melaniemozee@gmail.com>
2020-06-22 14:12:21 -0500

    Second unit test created to check if capacity increases

--- CustomListTest/UnitTest1.cs
+++ CustomListTest/UnitTest1.cs
@@ -22,7 +22,24 @@ namespace CustomListTest
             //Assert
             Assert.AreEqual(expectedValue, ourCustomList.count);
         }
-      
+        public void WasIndexIncreasedAfterFifthItemWasAdded()
+        {
+            //Arrange
+            CustomList<int> ourCustomList = new CustomList<int>();
+            int indexexpectedValue = 8;
+
+            //Act
+            ourCustomList.Add(6);
+            ourCustomList.Add(8);
+            ourCustomList.Add(12);
+            ourCustomList.Add(32);
+            ourCustomList.Add(7);
+
+
+            //Assert
+            Assert.AreEqual(indexexpectedValue, ourCustomList.capacity);
+        }
+        
 
     }
 }
