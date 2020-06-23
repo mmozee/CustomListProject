debe768ef32b8bd0bbe6fc7bb13c89f4e727a2bd
unknown <melaniemozee@gmail.com>
2020-06-23 08:59:20 -0500

    two tests changed with ReturnTrueIfItemInTheList and DoesTheItemShiftAfterRemovingItemFromTheMiddle

--- CustomListTest/RemoveMethodTest.cs
+++ CustomListTest/RemoveMethodTest.cs
@@ -48,7 +48,7 @@ namespace CustomListTest
             Assert.AreEqual(countExpectedValue, ourCustomList.count);
         }
         [TestMethod]
-        public void DoesCapacityDecreaseAfterRemovingFifthItem()
+        public void DoesTheItemsShiftAfterRemovingItemFromTheMiddle()
         {
             //Arrange
             CustomList<int> ourCustomList = new CustomList<int>();
@@ -57,41 +57,19 @@ namespace CustomListTest
             ourCustomList.Add(12);
             ourCustomList.Add(32);
             ourCustomList.Add(7);
-            int expectedCapacityValue = ourCustomList.capacity / 2;
-
-            //Act
-            ourCustomList.Remove(7);
-
-
-            //Assert
-            Assert.AreEqual(expectedCapacityValue, ourCustomList.capacity);
-
-        }
-        [TestMethod]
-        public void DoesIndexFourStayAtTheCorrectValueWhenFifthItemIsRemoved()
-        {
-            //Arrange
-            CustomList<int> ourCustomList = new CustomList<int>();
-            ourCustomList.Add(6);
-            ourCustomList.Add(8);
-            ourCustomList.Add(12);
-            ourCustomList.Add(32);
-            ourCustomList.Add(7);
-            int indexExpectedValue = 32;
+            int expectedValue =32;
             int value;
 
             //Act
-            ourCustomList.Remove(7);
-            value = ourCustomList[4];
-
-
-
+            ourCustomList.Remove(12);
+            value = ourCustomList[2];
+            
             //Assert
-            Assert.AreEqual(indexExpectedValue, value);
+            Assert.AreEqual(expectedValue,value);
 
         }
         [TestMethod]
-        public void IsTheItemInTheListReturnFalse()
+        public void IfTheItemIsNotInTheListReturnFalse()
         {
             //Arrange
             CustomList<int> ourCustomList = new CustomList<int>();
@@ -106,6 +84,28 @@ namespace CustomListTest
             //Act
             ourCustomList.Remove(4);
             isItemInList = false;
+
+
+
+            //Assert
+            Assert.AreEqual(expectedValue, isItemInList);
+        }
+        [TestMethod]
+        public void IfTheItemIsInTheListReturnTrue()
+        {
+            //Arrange
+            CustomList<int> ourCustomList = new CustomList<int>();
+            ourCustomList.Add(6);
+            ourCustomList.Add(8);
+            ourCustomList.Add(12);
+            ourCustomList.Add(32);
+            ourCustomList.Add(7);
+            bool isItemInList;
+            bool expectedValue = true;
+
+            //Act
+            ourCustomList.Remove(32);
+            isItemInList = true;
 
 
 
