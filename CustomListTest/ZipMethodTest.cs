519a374d9645e376f3a71b2cafe0946749abcca2
melaniemozee@gmail.com <melaniemozee@gmail.com>
2020-06-25 12:37:27 -0500

    Created ZipMethod Unit Tests

--- CustomListTest/ZipMethodTest.cs
+++ CustomListTest/ZipMethodTest.cs
@@ -8,47 +8,148 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CustomListTest
 {
-   [TestClass]
-   public class ZipMethodTest
+    [TestClass]
+    public class ZipMethodTest
     {
         [TestMethod]
-        public void DoesZipMethodTakeFirstItemOfTheListOne()
+        public void DoesZipMethodTakeFirstItemOfTheListOneAndBecomeTheZeroIndexForNewList()
         {
             //Arrange
+            CustomList<int> listOne = new CustomList<int>();
+            CustomList<int> listTwo = new CustomList<int>();
+            listOne.Add(6);
+            listOne.Add(8);
+            listOne.Add(12);
+            listOne.Add(32);
+            listOne.Add(7);
+
+            listTwo.Add(3);
+            listTwo.Add(4);
+            listTwo.Add(57);
+
+            int expectedValue;
+            int value;
+            
+
             //Act
+            expectedValue = listOne[0];
+            value = listOne.Zip(listTwo);
+
             //Assert
+            Assert.AreEqual(expectedValue, value[0]);
         }
 
         [TestMethod]
         public void DoesZipMethodTakeFirstItemOfTheListTwoAndPutItAfterListOneFirstItem()
-        {
-            //Arrange
+        {   //Arrange
+            CustomList<int> listOne = new CustomList<int>();
+            CustomList<int> listTwo = new CustomList<int>();
+            listOne.Add(6);
+            listOne.Add(8);
+            listOne.Add(12);
+            listOne.Add(32);
+            listOne.Add(7);
+
+            listTwo.Add(3);
+            listTwo.Add(4);
+            listTwo.Add(57);
+
+            int expectedValue;
+            int value;
+
+
             //Act
+            expectedValue = listTwo[0];
+            value = listOne.Zip(listTwo);
+
             //Assert
+            Assert.AreEqual(expectedValue, value[1]);
         }
 
         [TestMethod]
         public void DoesZipMethodMergeListsIntoANewList()
-        {
+        {   
             //Arrange
+            CustomList<int> listOne = new CustomList<int>();
+            CustomList<int> listTwo = new CustomList<int>();
+            listOne.Add(6);
+            listOne.Add(8);
+            listOne.Add(12);
+            listOne.Add(32);
+            listOne.Add(7);
+
+            listTwo.Add(3);
+            listTwo.Add(4);
+            listTwo.Add(57);
+
+            string expectedValue;
+            CustomList<int> value;
+
+
             //Act
+            expectedValue = "63841257327";
+            value = listOne.Zip(listTwo);
+
+
             //Assert
+            Assert.AreEqual(expectedValue, value.ToString());
         }
 
         [TestMethod]
         public void DoesMethodWorkIfTheItemsCountsDontMatch()
         {
             //Arrange
+            CustomList<int> listOne = new CustomList<int>();
+            CustomList<int> listTwo = new CustomList<int>();
+            listOne.Add(6);
+            listOne.Add(8);
+            listOne.Add(12);
+            listOne.Add(32);
+            listOne.Add(7);
+
+            listTwo.Add(3);
+            listTwo.Add(4);
+            listTwo.Add(57);
+
+            string expectedValue;
+            CustomList<int> value;
+
+
             //Act
+            expectedValue = "63841257327";
+            value = listOne.Zip(listTwo);
+
+
             //Assert
+            Assert.AreEqual(expectedValue, value.ToString());
         }
 
         [TestMethod]
         public void DidItemIndexesChangeToReflectNewLocationInTheList()
         {
             //Arrange
+            CustomList<int> listOne = new CustomList<int>();
+            CustomList<int> listTwo = new CustomList<int>();
+            listOne.Add(6);
+            listOne.Add(8);
+            listOne.Add(12);
+            listOne.Add(32);
+            listOne.Add(7);
+
+            listTwo.Add(3);
+            listTwo.Add(4);
+            listTwo.Add(57);
+
+            int expectedValue;
+            int value;
+
+
             //Act
+            expectedValue = listTwo[2];
+            value = listTwo.Zip(listOne);
+
             //Assert
+            Assert.AreEqual(expectedValue, value[4]);
         }
 
 
