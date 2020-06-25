93209981adb2b889ff495441e128d5fce363b70e
kadirermaya <kadir.ermaya@gmail.com>
2020-06-25 12:46:15 -0700

    Created a ZipMethodTest Class

---
+++ CustomListTest/ZipMethodTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListTest
+{
+   [TestClass]
+   public class ZipMethodTest
+    {
+        [TestMethod]
+        public void DoesZipMethodTakeFirstItemOfTheListOne()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void DoesZipMethodTakeFirstItemOfTheListTwoAndPutItAfterListOneFirstItem()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void DoesZipMethodMergeListsIntoANewList()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void DoesMethodWorkIfTheItemsCountsDontMatch()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        [TestMethod]
+        public void DidItemIndexesChangeToReflectNewLocationInTheList()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+
+    }
+}
